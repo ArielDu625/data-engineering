e75fc5d1a0a9acbdfd424b5d67f03a0abe57f669
Ariel <arieldu@qq.com>
2024-09-12 18:59:16 +0000

    add terraform and gcp

---
+++ docker-terraform/terraform_gcp/variables.tf
@@ -0,0 +1,34 @@
+variable "credentials" {
+  description = "My credentials"
+  default     = "./keys/ariel_gcp.json"
+}
+
+variable "project" {
+  description = "Project"
+  default     = "i-matrix-435319-g3"
+}
+
+variable "region" {
+  description = "Project Region"
+  default     = "us-central1"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "ny_taxi"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "i-matrix-435319-g3-terra-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
